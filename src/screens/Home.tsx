38ccd71eed3b600c281172c489f20ff6ea263566
WIllianMartins2018 <willian.martins01@hotmail.com>
2024-01-02 18:57:11 -0300

    Finalizando a Listagem de Grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,32 @@
-import { Center, Text } from 'native-base';
+import { VStack, FlatList } from 'native-base';
+import { HomeHeader } from '../components/HomeHeader';
+import { Group } from '../components/Group';
+import { useState } from 'react';
 
 export function Home() {
-    return(
-        <Center flex={1}>
-            <Text color="white">Home</Text>
-        </Center>
-    );
+  const [groupSelected, setGroupSelected] = useState<string>('costa');
+  const [groups, setGroups] = useState<string[]>(['costas', 'bíceps', 'tríceps', 'ombro']);
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{px: 8}}
+        my={10}
+        maxH={10}
+      />
+    </VStack>
+  );
 }
